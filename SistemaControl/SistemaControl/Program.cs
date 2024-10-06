0d3ee027249156249ae44f24caf0c6b94f830e6c
emily <gutierrezemilyalexandra@gmail.com>
2024-10-05 22:04:15 -0600

    ControlAsistencia

---
+++ SistemaControl/SistemaControl/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//PROGRAMA ELABORADO POR EMILY GUTIERREZ.
+namespace SistemaControl
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Universidad universidad = new Universidad();
+
+            universidad.RegistrarEstudiante(new Universidad.Estudiante("Roberto", "Gomez", 40, 28));
+            universidad.RegistrarEstudiante(new Universidad.Estudiante("Merlin", "D'Trinidad", 40, 37));
+            universidad.RegistrarEstudiante(new Universidad.Estudiante("Tony", "MiraFlores", 40, 18));
+            universidad.RegistrarEstudiante(new Universidad.Estudiante("Yuliza", "Castro", 40, 40));
+            universidad.RegistrarEstudiante(new Universidad.Estudiante("Cristopher", "Gutierrez", 40, 5));
+            universidad.RegistrarEstudiante(new Universidad.Estudiante("Ericka", "Martinez", 40, 32));
+            universidad.RegistrarEstudiante(new Universidad.Estudiante("Humberto", "RodriDguez", 40, 25));
+            universidad.RegistrarEstudiante(new Universidad.Estudiante("Jean", "Leon", 40, 11));
+            universidad.RegistrarEstudiante(new Universidad.Estudiante("Dafnne", "Jimenez", 40, 29));
+
+
+            foreach (Universidad.Estudiante estudiante in universidad.Estudiantes) 
+            {
+                estudiante.MostrarInformacion();
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ SistemaControl/SistemaControl/Universidad.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaControl
+{
+    public class Universidad
+    {
+        public List<Estudiante> Estudiantes { get; set; }
+
+        public Universidad()
+        {
+            Estudiantes = new List<Estudiante>();
+        }
+
+        public void RegistrarEstudiante(Estudiante estudiante)
+        {
+            Estudiantes.Add(estudiante);
+        }
+
+        public class Estudiante
+        {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public int NumeroSesionesTotales { get; set; }
+            public int NumeroSesionesAsistidas { get; set; }
+            public Asistencia Asistencia { get; set; }
+
+            public Estudiante(string nombre, string apellido, int numeroSesionesTotales, int numeroSesionesAsistidas)
+            {
+                Nombre = nombre;
+                Apellido = apellido;
+                NumeroSesionesTotales = numeroSesionesTotales;
+                NumeroSesionesAsistidas = numeroSesionesAsistidas;
+                Asistencia = new Asistencia(this);
+            }
+        }
+
+        public class Asistencia
+        {
+            private Estudiante estudiante;
+
+            public Asistencia(Estudiante estudiante)
+            {
+                this.estudiante = estudiante;
+            }
+
+            public static double CalcularPorcentajeAsistencia(Estudiante estudiante)
+            {
+                return (double)estudiante.NumeroSesionesAsistidas / estudiante.NumeroSesionesTotales * 100;
+            }
+
+            public static bool CumpleMinimoAsistencia(Estudiante estudiante)
+            {
+                return CalcularPorcentajeAsistencia(estudiante) >= 75;
+            }
+        }
+    }
+
+    public static class EstudianteExtensions
+    {
+        public static void MostrarInformacion(this Universidad.Estudiante estudiante) 
+        {
+            double porcentajeAsistencia = Universidad.Asistencia.CalcularPorcentajeAsistencia(estudiante);
+            bool cumpleMinimo = Universidad.Asistencia.CumpleMinimoAsistencia(estudiante);
+
+            Console.WriteLine($"Nombre: {estudiante.Nombre}");
+            Console.WriteLine($"Apellido: {estudiante.Apellido}");
+            Console.WriteLine($"Porcentaje de asistencia: {porcentajeAsistencia:F2}%");
+            Console.WriteLine($"Cumple con el mínimo de asistencia: {(cumpleMinimo ? "Sí" : "No")}");
+        }
+    }
+}
+
